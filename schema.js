37c101ec7540bcedf090108e282952435a82f968
Ezeikel Pemberton <ezeikelpemberton@gmail.com>
2018-09-12 00:52:22 +0100

    Removed dummy Book data.

--- schema.js
+++ schema.js
@@ -3,36 +3,13 @@ const mongoose = require('mongoose');
 
 const User = mongoose.model('User');
 
-// This is a (sample) collection of books we'll be able to query
-// the GraphQL server for.  A more complete example might fetch
-// from an existing data source like a REST API or database.
-const books = [{
-    title: 'Harry Potter and the Chamber of Secrets',
-    author: 'J.K. Rowling',
-  },
-  {
-    title: 'Jurassic Park',
-    author: 'Michael Crichton',
-  },
-];
-
-// Type definitions define the "shape" of your data and specify
-// which ways the data can be fetched from the GraphQL server.
+// defining "shape" of data
 module.exports.typeDefs = gql`
-  # Comments in GraphQL are defined with the hash (#) symbol.
-  # This "Book" type can be used in other type declarations.
-  type Book {
-    title: String
-    author: String
-  }
   # The "Query" type is the root of all GraphQL queries.
-  # (A "Mutation" type will be covered later on.)
   type Query {
-    books: [Book]
     users: [User]
   }
 
-  # my stuff
   type User {
     id: ID
     email: String
@@ -47,11 +24,9 @@ module.exports.typeDefs = gql`
   }
 `;
 
-// Resolvers define the technique for fetching the types in the
-// schema. We'll retrieve books from the "books" array above.
+// this is how "get" the data we need
 module.exports.resolvers = {
   Query: {
-    books: () => books,
     users: () => User.find()
   },
   Mutation: {
