1f1a2a3800d0bc001a81b0e1733ba7b10e865c2c
Ezeikel Pemberton <ezeikel_pemberton@discovery.com>
2018-09-13 17:09:33 +0100

    Added cors package. Trying to fix issue with cookie header not setting cookie on cross domain request.

--- app.js
+++ app.js
@@ -1,23 +1,32 @@
 const express = require('express');
+const cors = require('cors');
 const path = require('path');
 const morgan = require('morgan');
 const cookierParser = require('cookie-parser');
+const mongoose = require('mongoose');
+const User = mongoose.model('User');
 const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./schema');
 
 // create express app
 const app = express();
 
-// configure app to handle CORS
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
-  res.setHeader(
-      "Access-Control-Allow-Headers",
-      "X-Requested-With, content-type, Authorization"
-  );
-  next();
-});
+// enable cors
+const allowedOrigins = ['http://localhost:3000', 'https://five-yards.herokuapp.com'];
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (allowedOrigins.includes(origin)) {
+      console.log('Here!');
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  allowedHeaders: ['Access-Control-Allow-Headers', 'Origin,X-Requested-With, Content-Type, Accept'],
+  credentials: true
+}
+
+app.use(cors(corsOptions));
 
 // log all requests to the console
 if (process.env.SILENCE_LOGS !== "true") {
@@ -26,12 +35,25 @@ if (process.env.SILENCE_LOGS !== "true") {
 
 app.use(cookierParser());
 
+// check if token exists and if it does get the userId from it
 app.use((req, res, next) => {
   const { token } = req.cookies;
   if (token) {
     const { userId } = jwt.verify(token, process.env.APP_SECRET);
     req.userId = userId;
   }
+  next();
+});
+
+// get User from their id
+app.use(async (req, res, next) => {
+  console.log({userId: req.userId});
+
+  if (!req.userId) return next();
+
+  // bring back specific fields from User
+  const user = await User.find({ id: req.id }, { permissions: 1, email: 1, name: 1 });
+  req.user = user;
   next();
 });
 
