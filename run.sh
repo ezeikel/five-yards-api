8813d77722c35b28be31a2d2578fe282a92edd6e
Ezeikel Pemberton <hi@ezeikel.dev>
2021-08-09 21:55:17 +0100

    Updated setup script.

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 cd /home/ec2-user/five-yards-api
-pm2 restart five-yards-api
+pm2 start yarn --name "five-yards-api" --log-date-format "YYYY-MM-DD HH:mm" -- start
+pm2 startup
+pm2 save
