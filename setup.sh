ca829f44fb7546c78e04b4b9b5d9b63065be5c8e
Ezeikel Pemberton <hi@ezeikel.dev>
2021-08-09 21:24:24 +0100

    Updated setup script.

--- setup.sh
+++ setup.sh
@@ -2,6 +2,9 @@
 
 cd /home/ec2-user/five-yards-api
 source /home/ec2-user/.bashrc
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 npm install --global yarn
 yarn install
 yarn build
