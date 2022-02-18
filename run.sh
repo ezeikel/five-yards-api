95f559882dd510353cd74e06930fb9a4467bf28f
Ezeikel Pemberton <>
2022-02-18 09:59:35 +0000

    chore: removed code deploy config files.

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd /home/ec2-user/five-yards-api
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
-export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-nvm install node
-npm install pm2 -g
-pm2 restart "five-yards-api" --log-date-format "YYYY-MM-DD HH:mm"
